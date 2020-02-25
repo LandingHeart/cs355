c0fe10fe7c40731449b99aa4c811a558680df23f
LandingHeart <land.liu0123@gmail.com>
2020-02-25 17:51:28 -0500

    img

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import Navbar from "./component/Navbar";
 import About from "./component/About";
 import DisplayQuestionContainer from "./container/DisplayQuestionContainer";
 import AddQuestions from "./component/AddQuestions";
+import Home from "./container/Home";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route exact path="/"></Route>
+        <Route exact path="/" component={Home}></Route>
         <Route path="/about" component={About}></Route>
         <Route path="/questions" component={DisplayQuestionContainer}></Route>
         <Route path="/addQuestions" component={AddQuestions}></Route>
---
+++ frontend/src/container/Home.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import "./home.css";
+export default class Home extends Component {
+  render() {
+    return <div className="img">djkfakjfd;aksfj asdlfkjaskdlfj</div>;
+  }
+}
