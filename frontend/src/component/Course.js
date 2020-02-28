d938ff514d0c2994d5232a7d4358a0655f977de7
LandingHeart <land.liu0123@gmail.com>
2020-02-28 13:30:44 -0500

    card layout

--- frontend/src/component/Course.js
+++ frontend/src/component/Course.js
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 export default class Course extends Component {
   render() {
     return (
-      <div className="centered">
-        <section className="cards">
-          <article className="card">
-            <h3 className="card-title">Course: {this.props.coursename}</h3>
-            <Link to="/questions"> {this.props.coursename} </Link>
-          </article>
-        </section>
+      <div>
+        <div>
+          <div className="cards">
+            <div className="card">
+              <h3 className="card-title">Course: {this.props.coursename}</h3>
+              <Link to="/questions"> {this.props.coursename} </Link>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
