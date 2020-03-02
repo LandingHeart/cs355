172f573f5bc21eb2ab45f789d34487cf5f7741b7
LandingHeart <land.liu0123@gmail.com>
2020-03-02 09:28:07 -0500

    change navbar position

--- frontend/src/component/Course.js
+++ frontend/src/component/Course.js
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 export default class Course extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h3 className="card-title">Course: {this.props.coursename}</h3>
         <Link to="/questions"> {this.props.coursename} </Link>
+        <br />
+        <br />
+        <small>Join Code: 88888888 </small>
+        <br />
+        <small> Professor: Lawrence Titleman</small>
       </div>
     );
   }
