d675a31466e78684bad110054a9464e555a26eeb
LandingHeart <land.liu0123@gmail.com>
2020-04-29 14:24:31 -0400

    one moretry

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const router = express.Router();
 const path = require("path");
 const port = process.env.PORT || 8080;
-
+require("dotenv/config");
 //middleware, function when routes are hit
 const withAuth = require("./middleware");
 
@@ -21,21 +21,20 @@ router.get("/api/secret", withAuth, function (req, res) {
 const questionModel = require("./routes/question");
 const customerModel = require("./routes/customers");
 const courseModel = require("./routes/course");
-require("dotenv/config");
 
 app.use(bodyParser.json());
 app.use(cookieparser());
 app.use(cors());
-
-app.use(express.static("public"));
-app.use("/courses", courseModel);
-app.use("/questions", questionModel);
-app.use("/customers", customerModel);
 mongoose.connect(
   process.env.MONGOD_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("connected to port " + port)
 );
+app.use(express.static("public"));
+app.use("/courses", courseModel);
+app.use("/questions", questionModel);
+app.use("/customers", customerModel);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
 }
