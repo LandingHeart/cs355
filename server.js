e317aac97991e942f44ac5e98a06deafc5139c8d
LandingHeart <land.liu0123@gmail.com>
2020-04-29 12:58:31 -0400

    fix

--- server.js
+++ server.js
@@ -7,27 +7,14 @@ const app = express();
 const router = express.Router();
 const path = require("path");
 const port = process.env.PORT || 8080;
-
-//middleware, function when routes are hit
 const withAuth = require("./middleware");
+//middleware, function when routes are hit
 
-router.get("/checkToken", withAuth, function(req, res) {
-  res.sendStatus(200);
-});
-
-router.get("/api/secret", withAuth, function(req, res) {
-  res.send("password");
-});
 const questionModel = require("./routes/question");
 const customerModel = require("./routes/customers");
 const courseModel = require("./routes/course");
 require("dotenv/config");
 
-mongoose.connect(
-  process.env.MONGOD_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("connected to port " + port)
-);
 app.use(bodyParser.json());
 app.use(cookieparser());
 app.use(cors());
@@ -36,6 +23,22 @@ app.use(express.static("public"));
 app.use("/courses", courseModel);
 app.use("/questions", questionModel);
 app.use("/customers", customerModel);
+
+//////
+//middleware, function when routes are hit
+
+router.get("/api/secret", withAuth, function (req, res) {
+  res.send("password");
+});
+router.get("/api/secret", withAuth, function (req, res) {
+  res.send("password");
+});
+
+mongoose.connect(
+  process.env.MONGOD_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to port " + port)
+);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
